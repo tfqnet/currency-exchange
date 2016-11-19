95e45add9f83b8fe8e090dc9c4592a275dc66deb
Mohamad Taufiq <tfqnet@gmail.com>
2016-11-19 23:13:48 +0800

    Initial Commit

---
+++ Currency EXC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Currency EXC
+//
+//  Created by PETRONAS ICT SDN BHD on 19/11/2016.
+//  Copyright © 2016 TfqNet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Currency EXC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Currency EXC
+//
+//  Created by PETRONAS ICT SDN BHD on 19/11/2016.
+//  Copyright © 2016 TfqNet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
